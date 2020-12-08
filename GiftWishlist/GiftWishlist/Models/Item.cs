7af8e71603c6085038e924bfd0155ea32a649af9
veeepi <vpreduna90@gmail.com>
2020-12-08 10:22:54 -0800

    closes #16 Added Item Model

---
+++ GiftWishlist/GiftWishlist/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftWishlist.Models
+{
+    public class Wishlist
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime DueDate { get; set; }
+
+
+    }
+}
