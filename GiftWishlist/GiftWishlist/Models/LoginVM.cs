a7e872593686c8fba7be1d1db83c22fd7eadd7e1
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2020-12-08 10:44:25 -0800

    Scaffolded identity, and added a basic loginvm

---
+++ GiftWishlist/GiftWishlist/Models/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiftWishlist.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Rember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
